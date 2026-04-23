c77ff608e78e27c750c9ba08c1b910b66bd7b6d7
Rifki Virzya <114592995+rifchzschki@users.noreply.github.com>
2026-04-23 14:21:59 +0700

    Add: kth-largest-integer-in-a-stream - submission-3

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-3.cpp
@@ -0,0 +1,32 @@
+class KthLargest {
+private:
+    int k_;
+    priority_queue<int, vector<int>, greater<int>> pq_;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        k_ = k;
+        int it = 0;
+        while(k-- && it<nums.size()){
+            pq_.push(nums[it]);
+            it++;
+        }
+        while(it<nums.size()){
+            add(nums[it]);
+            it++;
+        }
+        // pq_ = priority_queue<int, vector<int>, greater<int>>(nums.begin(), nums.end());
+        // cout << pq_.top() << endl;
+    }
+    
+    int add(int val) {
+        if(pq_.size()<k_){
+            pq_.push(val);
+        }else{
+            if(val>pq_.top()){
+                pq_.pop();
+                pq_.push(val);
+            }
+        }
+        return pq_.top();
+    }
+};
