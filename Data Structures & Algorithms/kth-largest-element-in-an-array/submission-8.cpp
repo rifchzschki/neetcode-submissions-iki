7b10324b7686006280be8f58fcdb77fca78b71ef
Rifki Virzya <114592995+rifchzschki@users.noreply.github.com>
2026-04-27 19:26:46 +0700

    Add: kth-largest-element-in-an-array - submission-8

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-8.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int partition(vector<int>& arr, int l, int r){
+        int pivot = arr[l];
+        int i=l-1, j=r+1;
+        while(true){
+            do{
+                i++;
+            }while(arr[i]<pivot);
+            
+            do{
+                j--;
+            }while(arr[j]>pivot);
+
+            if(i>=j){
+                return j;
+            }
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    int quickSelect(vector<int>& arr, int l, int r, int k){
+        int pi = partition(arr, l, r);
+        if(l == r) return arr[l];
+        cout << l << " " << pi << " " << r << " " << k << endl;
+
+        if(pi>=k){
+            return quickSelect(arr, l, pi, k);
+        }else{
+            return quickSelect(arr, pi+1, r, k);
+        }
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        return quickSelect(nums, 0, nums.size()-1, nums.size()-k);
+    }
+};
