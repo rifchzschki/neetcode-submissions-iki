f3d99c9d8d1ed012af772e1671dbe71338bbd953
Rifki Virzya <114592995+rifchzschki@users.noreply.github.com>
2026-05-05 15:18:51 +0700

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int len = nums.size();
+        nums[1] = max(nums[0], nums[1]);
+        for(int i=2;i<len;i++){
+            nums[i] = max(nums[i-1], nums[i] + nums[i-2]);
+        }
+        return nums[len-1];
+
+    }
+};
