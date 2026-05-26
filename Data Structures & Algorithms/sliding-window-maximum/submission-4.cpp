7477f961053f6da22a02d21fe9c92ed5695b5e71
Rifki Virzya <114592995+rifchzschki@users.noreply.github.com>
2026-05-26 21:11:49 +0700

    Add: sliding-window-maximum - submission-4

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-4.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+        priority_queue<pair<int,int>> pq;
+
+        int i=0, j=0;
+        // while(j<k){
+        //     pq.push({nums[j], j});
+        //     j++;
+        // }
+        // res.push_back(pq.top().first);
+
+        while(j<nums.size()){
+            pq.push({nums[j], j});
+            if(j>=k){
+                while(!pq.empty() && pq.top().second <= i) pq.pop();
+                res.push_back(pq.top().first);
+                i++;
+            }
+            j++;
+            if(j==k) res.push_back(pq.top().first);
+        }
+        return res;
+    }
+};
