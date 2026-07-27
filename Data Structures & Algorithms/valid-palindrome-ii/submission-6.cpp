6e95b449bc2e5eba19c0494c60468cf0fc9f17df
Rifki Virzya <114592995+rifchzschki@users.noreply.github.com>
2026-07-28 04:13:50 +0700

    Add: valid-palindrome-ii - submission-6

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-6.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPal(string &s, int i, int j){
+        while(i<=j){
+            if(s[i]!=s[j]) return false;
+            i++;j--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int i=0,j = s.size()-1;
+        while(i<=j){
+            if(s[i]!=s[j]){
+                return isPal(s, i+1, j) || isPal(s, i, j-1);
+            }
+            i++;
+            j--;
+            
+        }
+        return true;
+    }
+};
