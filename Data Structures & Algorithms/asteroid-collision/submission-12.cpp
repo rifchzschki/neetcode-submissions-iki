b5c5b7e1589b76227b616341f84c4e5f52fa775b
Rifki Virzya <114592995+rifchzschki@users.noreply.github.com>
2026-07-28 02:15:53 +0700

    Add: asteroid-collision - submission-12

---
+++ Data Structures & Algorithms/asteroid-collision/submission-12.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isColl(int a, int b){
+        if(a>0 && b<0) return true; 
+        else return false;
+    }
+
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        deque<int> dq;
+        dq.push_back(asteroids[0]);
+        for(int i = 1;i<asteroids.size();i++){
+            int ast = asteroids[i];
+            while(!dq.empty() && isColl(dq.back(), ast) && abs(dq.back())<=abs(ast)){
+                int backVal = dq.back();
+                dq.pop_back();
+                if(abs(backVal)==abs(ast)){
+                    ast = 0;
+                    break;
+                }
+            }
+            if(ast && (dq.empty() || !isColl(dq.back(), ast))){
+                dq.push_back(ast);
+            }
+
+        }
+        return vector<int>(dq.begin(), dq.end());
+    }
+};
+
+
