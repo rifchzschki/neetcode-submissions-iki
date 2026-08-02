0872dfe536b05eedbf6ce916eac102afe901a674
Rifki Virzya <114592995+rifchzschki@users.noreply.github.com>
2026-08-02 19:11:24 +0700

    Add: excel-sheet-column-title - submission-6

---
+++ Data Structures & Algorithms/excel-sheet-column-title/submission-6.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    // iterative
+    string convertToTitle(int columnNumber) {
+        string ans = "";
+        while(columnNumber){
+            columnNumber--;
+            ans += char((columnNumber%26)+'A'); 
+            columnNumber/=26;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
